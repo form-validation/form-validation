e9b3d93a3d315a2fa6eef566ce6d47583de30829
phuocng <me@phuoc.ng>
2019-05-28 20:40:16 +0700

    Add CSS frameworks section

---
+++ src/components/DotDotDot.svelte
@@ -0,0 +1,22 @@
+<style>
+.dot {
+    border-radius: 9999px;
+    display: block;
+    height: 8px;
+}
+.dot:nth-child(1) {
+    background-color: #f08a5d;
+}
+.dot:nth-child(2) {
+    background-color: #b83b5e;
+}
+.dot:nth-child(3) {
+    background-color: #f9ed69;
+}
+</style>
+
+<div class="flex">
+    <div class="dot mr1 w4"></div>
+    <div class="dot mr1 w2"></div>
+    <div class="dot w1"></div>
+</div>
