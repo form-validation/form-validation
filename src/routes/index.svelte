1cf3fbc9d3e949528d85b50f5f25204b79a53802
phuocng <me@phuoc.ng>
2019-05-29 09:30:55 +0700

    Add footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -615,6 +615,41 @@ formValidation(formElement, {
     </div>
 </section>
 
+<section class="bt b--black-30 pv5">
+    <div class="mw8 flex center">
+        <div class="w-40">
+            <div class="f4 fw6 mv3">FormValidation</div>
+            <ul class="list ma0 pa0 lh-copy">
+                <li><a href="/guide/getting-started/" class="link">Getting Started</a></li>
+                <li><a href="/guide/validators/" class="link">Validators</a></li>
+                <li><a href="/guide/plugins/" class="link">Plugins</a></li>
+                <li><a href="/guide/api/" class="link">API</a></li>
+                <li><a href="/guide/examples/" class="link">Examples</a></li>
+            </ul>
+        </div>
+        <div class="w-30">
+            <div class="f4 fw6 mv3">Toys</div>
+            <ul class="list ma0 pa0 lh-copy">
+                <li><a href="https://blur.page" class="link" target="_blank">BlurPage</a></li>
+            </ul>
+        </div>
+        <div class="w-30">
+            <div class="f4 fw6 mv3">About</div>
+            <div class="pb2">&copy; 2013 &mdash; 2019 Nguyen Huu Phuoc</div>
+            <div class="pb2">all rights reserved.</div>
+            <div class="pb2">handcrafted with <span class="dark-red"><i class="fa fa-heart"></i></span></div>
+            <div class="flex items-center">
+                <a href="https://twitter.com/nghuuphuoc" class="dib dim link black-70 ba b-black-70 br-pill w2 h2 flex items-center justify-center mr2">
+                    <svg class="w-60 h-60" viewBox="0 0 24 24" style="stroke: rgb(0, 0, 0); fill: none; stroke-width: 1; stroke-linecap: round; stroke-linejoin: round"><path d="M23,6.628l-2-0.5l1-2l-2.464,0.7c-1.809-1.688-4.644-1.589-6.332,0.22c-0.78,0.836-1.21,1.938-1.204,3.08v1 c-3.539,0.73-6.634-1.2-9.5-4.5c-0.5,2.667,0,4.667,1.5,6l-3-0.5c0.405,2.069,1.362,3.7,4,4l-2.5,1c1,2,2.566,2.31,5,2.5 c-1.893,1.353-4.174,2.054-6.5,2c12.755,5.669,20-2.664,20-10V8.3L23,6.628z"></path></svg>
+                </a>
+                <a href="mailto: me@phuoc.ng" class="dib dim link black-70 ba b-black-70 br-pill w2 h2 flex items-center justify-center">
+                    <svg class="w-60 h-60" viewBox="0 0 24 24" style="stroke: rgb(0, 0, 0); fill: none; stroke-width: 1; stroke-linecap: round; stroke-linejoin: round"><path d="M2.5,3.954h19c1.105,0,2,0.895,2,2v12c0,1.105-0.895,2-2,2h-19c-1.105,0-2-0.895-2-2v-12 C0.5,4.849,1.395,3.954,2.5,3.954z M22.911,4.536L12,13.454L1.089,4.536"></path></svg>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
 <script>
 import Typed from '../_demo/typed/Tachyons.svelte';
 import DotDotDot from '../components/DotDotDot.svelte';
