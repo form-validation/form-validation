d8f685574bd4e1bcd8dee4db53b151532ccf707e
phuocng <me@phuoc.ng>
2019-06-07 00:40:19 +0700

    Add Special field name page

---
+++ src/routes/guide/getting-started/special-field-name/basic/Tachyons.svelte
@@ -0,0 +1,118 @@
+<TachyonsLayout>
+    <form id="demoForm" method="post">
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">Full name</div>
+                <div class="fl w-30 mr2">
+                    <input type="text" name="user[firstName]" placeholder="First name" class="input-reset ba b--black-20 pa2 mb2 db w-100" />
+                </div>
+                <div class="fl w-30">
+                    <input type="text" name="user[lastName]" placeholder="Last name" class="input-reset ba b--black-20 pa2 mb2 db w-100" />
+                </div>
+            </div>
+        </div>
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">Username</div>
+                <div class="fl w-40">
+                    <input type="text" name="user[username]" class="input-reset ba b--black-20 pa2 mb2 db w-100" />
+                </div>
+            </div>
+        </div>
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">Email address</div>
+                <div class="fl w-40">
+                    <input type="text" name="user[email]" class="input-reset ba b--black-20 pa2 mb2 db w-100" />
+                </div>
+            </div>
+        </div>
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2"></div>
+                <div class="fl w-50">
+                    <button type="submit" class="ba b--black-20 bg-blue white ph3 pv2 br2">Submit</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</TachyonsLayout>
+
+<script>
+import { onMount } from 'svelte';
+
+import formValidation from 'formvalidation/es6/core/Core';
+import DemoFrame from 'formvalidation/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/es6/plugins/Icon';
+import Trigger from 'formvalidation/es6/plugins/Trigger';
+import Tachyons from 'formvalidation/es6/plugins/Tachyons';
+import SubmitButton from 'formvalidation/es6/plugins/SubmitButton';
+
+import sampleCode from './Tachyons.programmatic';
+import TachyonsLayout from '../../../../../components/demo/TachyonsLayout.svelte';
+
+onMount(() => {
+    const fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            'user[firstName]': {
+                validators: {
+                    notEmpty: {
+                        message: 'The first name is required'
+                    }
+                }
+            },
+            'user[lastName]': {
+                validators: {
+                    notEmpty: {
+                        message: 'The last name is required'
+                    }
+                }
+            },
+            'user[username]': {
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: 'The username must be more than 6 and less than 30 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_\.]+$/,
+                        message: 'The username can only consist of alphabetical, number, dot and underscore'
+                    }
+                }
+            },
+            'user[email]': {
+                validators: {
+                    notEmpty: {
+                        message: 'The email address is required'
+                    },
+                    emailAddress: {
+                        message: 'The input is not a valid email address'
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            tachyons: new Tachyons(),
+            submitButton: new SubmitButton(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh',
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/getting-started/special-field-name/basic/Tachyons',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+
+    return () => {
+        fv.destroy();
+    };
+});
+</script>
