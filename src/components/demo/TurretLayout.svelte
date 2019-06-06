19b683948d243256ae841a7b7a7c0cf97c08d773
phuocng <me@phuoc.ng>
2019-06-06 22:32:32 +0700

    Fix layout issue for TurretCSS demo

--- src/components/demo/TurretLayout.svelte
+++ src/components/demo/TurretLayout.svelte
@@ -8,4 +8,10 @@ export {
 };
 </script>
 
+<style>
+:global(form) {
+    padding: 0 1rem;
+}
+</style>
+
 <ResourceLoader urls={['https://cdnjs.cloudflare.com/ajax/libs/bigfishtv-turret/5.1.3/turretcss.min.css']} onLoaded={onLoaded}><slot></slot></ResourceLoader>
