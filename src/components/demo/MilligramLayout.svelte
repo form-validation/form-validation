ae737d0f908fbcba30bf67efb9d05f40334fe713
phuocng <me@phuoc.ng>
2019-06-06 22:06:19 +0700

    Fix the scroll in Miligram demo

--- src/components/demo/MilligramLayout.svelte
+++ src/components/demo/MilligramLayout.svelte
@@ -8,4 +8,14 @@ export {
 };
 </script>
 
+<style>
+:global(body) {
+    margin: 0;
+}
+:global(form) {
+    margin-bottom: 0;
+    padding: 0 1rem;
+}
+</style>
+
 <ResourceLoader urls={['https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css']} onLoaded={onLoaded}><slot></slot></ResourceLoader>
