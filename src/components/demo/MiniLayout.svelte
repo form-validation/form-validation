cc16e655fdab3b9eb37bbb38b60295dd6bb2db03
phuocng <me@phuoc.ng>
2019-06-06 22:08:24 +0700

    Fix the scroll in Mini.css demo

--- src/components/demo/MiniLayout.svelte
+++ src/components/demo/MiniLayout.svelte
@@ -8,4 +8,10 @@ export {
 };
 </script>
 
+<style>
+:global(form) {
+    padding: 0;
+}
+</style>
+
 <ResourceLoader urls={['https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css']} onLoaded={onLoaded}><slot></slot></ResourceLoader>
