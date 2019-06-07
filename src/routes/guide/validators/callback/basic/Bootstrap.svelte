d51e3178c8ed4e70a327f2c9bf51492a66d04675
phuocng <me@phuoc.ng>
2019-06-07 17:07:11 +0700

    Add callback validator page

---
+++ src/routes/guide/validators/callback/basic/Bootstrap.svelte
@@ -0,0 +1,68 @@
+<BootstrapLayout onLoaded={onLoaded}>
+    <form id="demoForm" method="POST">
+        <div class="form-group row">
+            <label class="col-sm-3 col-form-label"><div id="captchaOperation"></div></label>
+            <div class="col-sm-5">
+                <input type="text" class="form-control" name="captcha" />
+            </div>
+        </div>
+    </form>
+</BootstrapLayout>
+
+<script>
+import { onDestroy } from 'svelte';
+
+import formValidation from 'formvalidation/es6/core/Core';
+import DemoFrame from 'formvalidation/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/es6/plugins/Icon';
+import Trigger from 'formvalidation/es6/plugins/Trigger';
+import Bootstrap from 'formvalidation/es6/plugins/Bootstrap';
+
+import sampleCode from './Bootstrap.programmatic';
+import BootstrapLayout from '../../../../../components/demo/BootstrapLayout.svelte';
+
+let fv;
+
+const onLoaded = () => {
+    // Generate a simple captcha
+    const randomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    const random = [randomNumber(1, 100), randomNumber(1, 200)];
+    document.getElementById('captchaOperation').innerHTML = [random[0], '+', random[1], '='].join(' ');
+
+    fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            captcha: {
+                validators: {
+                    callback: {
+                        message: 'Wrong answer',
+                        callback: (input) => {
+                            const items = document.getElementById('captchaOperation').innerHTML.split(' ');
+                            const sum = parseInt(items[0]) + parseInt(items[2]);
+                            return input.value == sum;
+                        }
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            bootstrap: new Bootstrap(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh'
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/validators/callback/basic/Bootstrap',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+};
+
+onDestroy(() => {
+    fv && fv.destroy();
+});
+</script>
---
+++ src/routes/guide/validators/callback/basic/Tachyons.svelte
@@ -0,0 +1,70 @@
+<TachyonsLayout>
+    <form id="demoForm" method="post">
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2"><div id="captchaOperation"></div></div>
+                <div class="fl w-50">
+                    <input type="text" name="captcha" class="input-reset ba b--black-20 pa2 mb2 db w-100" />
+                </div>
+            </div>
+        </div>
+    </form>
+</TachyonsLayout>
+
+<script>
+import { onMount } from 'svelte';
+
+import formValidation from 'formvalidation/es6/core/Core';
+import DemoFrame from 'formvalidation/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/es6/plugins/Icon';
+import Trigger from 'formvalidation/es6/plugins/Trigger';
+import Tachyons from 'formvalidation/es6/plugins/Tachyons';
+
+import sampleCode from './Tachyons.programmatic';
+import TachyonsLayout from '../../../../../components/demo/TachyonsLayout.svelte';
+
+let fv;
+
+onMount(() => {
+    // Generate a simple captcha
+    const randomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    const random = [randomNumber(1, 100), randomNumber(1, 200)];
+    document.getElementById('captchaOperation').innerHTML = [random[0], '+', random[1], '='].join(' ');
+
+    fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            captcha: {
+                validators: {
+                    callback: {
+                        message: 'Wrong answer',
+                        callback: (input) => {
+                            const items = document.getElementById('captchaOperation').innerHTML.split(' ');
+                            const sum = parseInt(items[0]) + parseInt(items[2]);
+                            return input.value == sum;
+                        }
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            tachyons: new Tachyons(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh'
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/validators/callback/basic/Tachyons',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+
+    return () => {
+        fv.destroy();
+    };
+});
+</script>
