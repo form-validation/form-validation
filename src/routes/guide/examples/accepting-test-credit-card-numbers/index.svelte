2abca2f2ff2abfaa15c4599664e7777d2272137a
phuocng <me@phuoc.ng>
2019-06-08 10:59:54 +0700

    Add Accepting test credit card numbers example

---
+++ src/routes/guide/examples/accepting-test-credit-card-numbers/index.svelte
@@ -0,0 +1,41 @@
+<svelte:head>
+	<title>FormValidation • Field selector</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">Examples</h1>
+    <h2 class="f4 fw4 tc">Accepting test credit card numbers</h2>
+    
+    <section class="mv5">
+        <p class="lh-copy">Do you want some of fake, even invalid credit card numbers to be valid? For example, in the developing phase,
+            you might want the creditCard validator to accept a few of particular card numbers.</p>
+        <p class="lh-copy">Of course, you don't need to modify the source code of creditCard validator and add your own numbers there.
+            FormValidation allows you to do it in a nice way via the <a href="/guide/plugins/transformer/" class="blue dim link">Transformer</a> plugin.</p>
+        <p class="lh-copy">By using this option, we transform your test card numbers from an invalid to a valid one.</p>
+
+        <Tip heading="Note">
+            The <a href="/guide/plugins/transformer/" class="blue dim link">Transformer</a> plugin changes the card numbers for validation process only.
+            When the form is submitted, you get the extract number that is initially provided by the user
+        </Tip>
+
+        <p class="lh-copy">Test (invalid) credit card numbers that you want it to be valid:</p>
+        <ul class="ma0 pl3 lh-copy">
+            <li>3333222233332222</li>
+            <li>30030008444444</li>
+        </ul>
+
+        <Demo prefix="/guide/examples/accepting-test-credit-card-numbers" frameworks={['Tachyons']} />
+    </section>
+
+    <Examples examples={['Asking credit card number to match with selected type', 'Showing credit card icon']} />
+
+    <RelatedValidators validators={['creditCard']} />
+</GuideLayout>
+
+<script>
+import Demo from '../../../../components/Demo.svelte';
+import Examples from '../../../../components/Examples.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import RelatedValidators from '../../../../components/RelatedValidators.svelte';
+import Tip from '../../../../components/Tip.svelte';
+</script>
