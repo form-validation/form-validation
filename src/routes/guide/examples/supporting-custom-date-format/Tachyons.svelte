627688afbba50ee221ccc180df29592b1fdbecd5
phuocng <me@phuoc.ng>
2019-06-08 15:08:51 +0700

    Add Supporting custom date format example

---
+++ src/routes/guide/examples/supporting-custom-date-format/Tachyons.svelte
@@ -0,0 +1,68 @@
+<TachyonsLayout>
+    <ResourceLoader urls={['https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.1/moment.min.js']} onLoaded={onLoaded}>
+        <form id="demoForm" method="post">
+            <div class="cf mb2">
+                <div class="fl w-100">
+                    <div class="fl w-50 pa2">What's US independence day?</div>
+                    <div class="fl w-50">
+                        <input type="text" name="independenceDay" placeholder="MMM D" class="input-reset ba b--black-20 pa2 mb2 db w-100" />
+                    </div>
+                </div>
+            </div>
+        </form>
+    </ResourceLoader>
+</TachyonsLayout>
+
+<script>
+import { onDestroy } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Tachyons from 'formvalidation/dist/es6/plugins/Tachyons';
+
+import sampleCode from './Tachyons.programmatic';
+import ResourceLoader from '../../../../components/ResourceLoader.svelte';
+import TachyonsLayout from '../../../../components/demo/TachyonsLayout.svelte';
+
+let fv;
+
+const onLoaded = () => {
+    fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            independenceDay: {
+                validators: {
+                    callback: {
+                        message: 'Wrong answer',
+                        callback: (input) => {
+                            const value = input.value;
+                            const m = new moment(value, 'MMMM D', true);
+                            return m.isValid() && m.month() === 6 && m.date() === 4;
+                        }
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            tachyons: new Tachyons(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh'
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/examples/supporting-custom-date-format/Tachyons',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+};
+
+onDestroy(() => {
+    if (fv) {
+        fv.destroy();
+    }
+});
+</script>
---
+++ src/routes/guide/examples/supporting-custom-date-format/index.svelte
@@ -0,0 +1,29 @@
+<svelte:head>
+	<title>FormValidation • Field selector</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">Examples</h1>
+    <h2 class="f4 fw4 tc">Supporting custom date format</h2>
+    
+    <section class="mv5">
+        <p class="lh-copy">This example illustrates the ability of validating date in custom format by using the <a href="/guide/validators/callback/" class="blue dim link">callback</a> validator and <a href="http://momentjs.com/" class="blue dim link">momentjs</a> to parse/validate.</p>
+        <Demo prefix="/guide/examples/supporting-custom-date-format" frameworks={['Tachyons']} />
+    </section>
+
+    <Examples examples={[
+        'Disabling date',
+        'Integrating with flatpickr',
+        'Supporting custom date format',
+        'Validating date range'
+    ]} />
+
+    <RelatedValidators validators={['date']} />
+</GuideLayout>
+
+<script>
+import Demo from '../../../../components/Demo.svelte';
+import Examples from '../../../../components/Examples.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import RelatedValidators from '../../../../components/RelatedValidators.svelte';
+</script>
