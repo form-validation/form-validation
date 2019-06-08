c68208a8c09b2f177df58bf1a89276aafb5c087f
phuocng <me@phuoc.ng>
2019-06-08 17:43:21 +0700

    Add Grid validator page

---
+++ src/routes/guide/validators/grid/index.svelte
@@ -0,0 +1,116 @@
+<svelte:head>
+	<title>FormValidation • grid validator</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">grid validator</h1>
+    <h2 class="f4 fw4 tc">Validate a <a href="https://en.wikipedia.org/wiki/Global_Release_Identifier" class="blue dim link">GRId (Global Release Identifier)</a></h2>
+
+    <section class="mv5">
+        <Heading>Options</Heading>
+
+        <h3>Using with form field</h3>
+        <p class="lh-copy i">The HTML attributes are used to set the validator options via the <a href="/guide/plugins/declarative/" class="blue dim link">Declarative</a> plugin</p>
+        <table class="collapse ba br2 b--black-10 pv2 ph3 w-100">
+            <tr class="striped--light-gray">
+                <th class="pv2 ph3 tl f6 fw6 ttu">Name</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">HTML attribute</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">Type</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">Description</th>
+            </tr>
+            <tr class="striped--light-gray">
+                <td class="pv2 ph3"><code>message</code></td>
+                <td class="pv2 ph3"><code>data-fv-grid___message</code></td>
+                <td class="pv2 ph3">String</td>
+                <td class="pv2 ph3">The error message</td>
+            </tr>
+        </table>
+
+        <h3>Using with ES6 module</h3>
+<SampleCode lang="javascript" code={`
+// You might need to change the importing path
+import grid from 'formvalidation/dist/es6/validators/grid';
+
+const result = grid().validate({
+    value: ...,
+    options: {
+        message: ...,
+    },
+});
+/*
+result is an object of
+{
+    valid: true or false,
+    message: The error message     
+}
+*/
+`} />
+    </section>
+
+    <section class="mv5">
+        <Heading>Basic Example</Heading>
+
+        <p class="lh-copy">You can click on sample to see if it's a valid or invalid GRId.</p>
+        <table class="collapse ba br2 b--black-10 pv2 ph3 w-100 mb4">
+            <tr class="striped--light-gray">
+                <th class="pv2 ph3 tl f6 fw6 ttu">Sample</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">Is valid?</th>
+            </tr>
+            {#each _samples as sample}
+            <tr class="striped--light-gray">
+                <SampleData sample={sample} sender="/guide/validators/grid/basic" />
+            </tr>
+            {/each}
+        </table>
+
+        <Demo prefix="/guide/validators/grid/basic" frameworks={['Bootstrap', 'Tachyons']} />
+    </section>
+
+    <section class="mv5">
+        <Heading>ES6 Module Example</Heading>
+
+        <p class="lh-copy">The following snippet shows how to use the grid validator with ES6 module:</p>
+<SampleCode lang="javascript" code={`
+// You might need to change the importing path
+import grid from 'formvalidation/dist/es6/validators/grid';
+
+const res1 = grid().validate({
+    value: 'A12425GABC1234002M',
+    options: {
+        message: 'The value is not valid GRId',
+    },
+});
+// res1.valid === true
+
+const res2 = grid().validate({
+    value: 'A1-2425G-ABC1234002-Q',
+    options: {
+        message: 'The value is not valid GRId',
+    },
+});
+// res2.valid === false
+`} />
+    </section>
+
+    <RelatedValidators validators={['ismn']} />
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/validators/greater-than/">greaterThan validator</PrevButton>
+            <NextButton href="/guide/validators/hex/">hex validator</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import Demo from '../../../../components/Demo.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import Heading from '../../../../components/Heading.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import RelatedValidators from '../../../../components/RelatedValidators.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+import SampleData from '../../../../components/SampleData.svelte';
+
+const _samples = ['A12425GABC1234002M', 'A1-2425G-ABC1234002-M', 'A1 2425G ABC1234002 M', 'Grid:A1-2425G-ABC1234002-M', 'A1-2425G-ABC1234002-Q'];
+</script>
