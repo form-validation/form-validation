f961b3bd26efb39f191890f9ae05026508c223ea
phuocng <me@phuoc.ng>
2019-06-08 17:32:51 +0700

    Add imo validator page

---
+++ src/routes/guide/validators/imo/basic/Tachyons.svelte
@@ -0,0 +1,81 @@
+<TachyonsLayout>
+    <ReceiveMessage channel="SAMPLE_FIELD_VALUE" sender="/guide/validators/imo/basic" on:received={receive}>
+        <form id="demoForm" method="post">
+            <div class="cf mb2">
+                <div class="fl w-100">
+                    <div class="fl w-25 pa2">IMO</div>
+                    <div class="fl w-50">
+                        <input type="text" bind:this={inputEle} name="imo" class="input-reset ba b--black-20 pa2 mb2 db w-100" />
+                    </div>
+                </div>
+            </div>
+        </form>
+    </ReceiveMessage>
+</TachyonsLayout>
+
+<script>
+import { onMount } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Tachyons from 'formvalidation/dist/es6/plugins/Tachyons';
+import imo from 'formvalidation/dist/es6/validators/imo';
+
+import sampleCode from './Tachyons.programmatic';
+import ReceiveMessage from '../../../../../components/ReceiveMessage.svelte';
+import TachyonsLayout from '../../../../../components/demo/TachyonsLayout.svelte';
+
+let fv;
+let inputEle;
+
+const receive = (e) => {
+    const v = e.detail.data;
+    inputEle.value = v;
+
+    if (fv) {
+        fv.revalidateField('imo').then((result) => {
+            window.parent.postMessage({
+                channel: 'DEMO_VALIDATE_RESULT',
+                sender: '/guide/validators/imo/basic',
+                data: {
+                    input: v,
+                    output: result
+                },
+            }, '*');
+        });
+    }
+};
+
+onMount(() => {
+    fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            imo: {
+                validators: {
+                    imo: {
+                        message: 'The value is not valid IMO'
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            tachyons: new Tachyons(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh'
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/validators/imo/basic/Tachyons',
+                sampleCode: sampleCode,
+            }),
+        },
+    }).registerValidator('imo', imo);
+
+    return () => {
+        fv.destroy();
+    };
+});
+</script>
