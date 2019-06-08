62bc46e5fbe3aae5fee2c335fc4d9df8a9617539
phuocng <me@phuoc.ng>
2019-06-08 17:35:57 +0700

    Add imei validator

---
+++ src/routes/guide/validators/imei/basic/Bootstrap.svelte
@@ -0,0 +1,79 @@
+<BootstrapLayout onLoaded={onLoaded}>
+    <ReceiveMessage channel="SAMPLE_FIELD_VALUE" sender="/guide/validators/imei/basic" on:received={receive}>
+        <form id="demoForm" method="POST">
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">IMEI</label>
+                <div class="col-sm-5">
+                    <input type="text" bind:this={inputEle} class="form-control" name="imei" />
+                </div>
+            </div>
+        </form>
+    </ReceiveMessage>
+</BootstrapLayout>
+
+<script>
+import { onDestroy } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Bootstrap from 'formvalidation/dist/es6/plugins/Bootstrap';
+import imei from 'formvalidation/dist/es6/validators/imei';
+
+import sampleCode from './Bootstrap.programmatic';
+import BootstrapLayout from '../../../../../components/demo/BootstrapLayout.svelte';
+import ReceiveMessage from '../../../../../components/ReceiveMessage.svelte';
+
+let fv;
+let inputEle;
+
+const receive = (e) => {
+    const v = e.detail.data;
+    inputEle.value = v;
+
+    if (fv) {
+        fv.revalidateField('imei').then((result) => {
+            window.parent.postMessage({
+                channel: 'DEMO_VALIDATE_RESULT',
+                sender: '/guide/validators/imei/basic',
+                data: {
+                    input: v,
+                    output: result
+                },
+            }, '*');
+        });
+    }
+};
+
+const onLoaded = () => {
+    fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            imei: {
+                validators: {
+                    imei: {
+                        message: 'The value is not valid IMEI'
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            bootstrap: new Bootstrap(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh'
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/validators/imei/basic/Bootstrap',
+                sampleCode: sampleCode,
+            }),
+        },
+    }).registerValidator('imei', imei);
+};
+
+onDestroy(() => {
+    fv && fv.destroy();
+});
+</script>
