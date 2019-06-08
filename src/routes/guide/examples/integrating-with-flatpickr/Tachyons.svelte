711283de26f64bdf265432d2cff16ec59ec3fabc
phuocng <me@phuoc.ng>
2019-06-08 15:04:13 +0700

    Add Integrating with flatpickr example

---
+++ src/routes/guide/examples/integrating-with-flatpickr/Tachyons.svelte
@@ -0,0 +1,78 @@
+<TachyonsLayout>
+    <ResourceLoader urls={[
+        'https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.5.0/flatpickr.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.5.0/flatpickr.min.js',
+    ]} onLoaded={onLoaded}>
+        <form id="demoForm" method="post" style="height: 380px;">
+            <div class="cf mb2">
+                <div class="fl w-100">
+                    <div class="fl w-25 pa2">Meeting time</div>
+                    <div class="fl w-50">
+                        <input type="text" name="meetingTime" placeholder="YYYY/MM/DD h:m A" class="input-reset ba b--black-20 pa2 mb2 db w-100" />
+                    </div>
+                </div>
+            </div>
+        </form>
+    </ResourceLoader>
+</TachyonsLayout>
+
+<script>
+import { onDestroy } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Tachyons from 'formvalidation/dist/es6/plugins/Tachyons';
+
+import sampleCode from './Tachyons.programmatic';
+import ResourceLoader from '../../../../components/ResourceLoader.svelte';
+import TachyonsLayout from '../../../../components/demo/TachyonsLayout.svelte';
+
+let fv;
+
+const onLoaded = () => {
+    const form = document.getElementById('demoForm');
+    fv = formValidation(form, {
+        fields: {
+            meetingTime: {
+                validators: {
+                    date: {
+                        format: 'YYYY/MM/DD h:m A',
+                        message: 'The value is not a valid date',
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            tachyons: new Tachyons(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh'
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/examples/integrating-with-flatpickr/Tachyons',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+
+    flatpickr(form.querySelector('[name="meetingTime"]'), {
+        enableTime: true,
+        // See https://flatpickr.js.org/formatting/
+        dateFormat: 'Y/m/d H:i K',
+        // After selecting a date, we need to revalidate the field
+        onChange: () => {
+            fv.revalidateField('meetingTime');
+        },
+    });
+};
+
+onDestroy(() => {
+    if (fv) {
+        fv.destroy();
+    }
+});
+</script>
---
+++ src/routes/guide/examples/integrating-with-flatpickr/index.svelte
@@ -0,0 +1,49 @@
+<svelte:head>
+	<title>FormValidation • Field selector</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">Examples</h1>
+    <h2 class="f4 fw4 tc">Integrating with flatpickr</h2>
+    
+    <section class="mv5">
+        <p class="lh-copy">The form below is an example of using the date validator with <a href="https://flatpickr.js.org/" class="blue dim link">flatpickr</a>, a lightweight and powerful datetime picker.</p>
+        <Demo prefix="/guide/examples/integrating-with-flatpickr" frameworks={['Tachyons']} />
+
+        <p class="lh-copy">If you look at the sample code in the <span class="i">Code</span> tab, the date field has to be revalidated after picking a date from the date picker. You have to do the same thing for any date picker.</p>
+        <SampleCode lang="javascript" code={`
+const fv = FormValidation.formValidation(form, {
+    fields: {
+        meetingTime: {
+            ...
+        },
+    },
+});
+
+flatpickr(form.querySelector('[name="meetingTime"]'), {
+    ...
+    // After selecting a date, we need to revalidate the field
+    onChange: function() {
+        fv.revalidateField('meetingTime');
+    },
+});
+`} />  
+    </section>
+
+    <Examples examples={[
+        'Disabling date',
+        'Integrating with flatpickr',
+        'Supporting custom date format',
+        'Validating date range'
+    ]} />
+
+    <RelatedValidators validators={['date']} />
+</GuideLayout>
+
+<script>
+import Demo from '../../../../components/Demo.svelte';
+import Examples from '../../../../components/Examples.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import RelatedValidators from '../../../../components/RelatedValidators.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+</script>
