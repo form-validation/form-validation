f306da3fe1fdc3b82e6bf938034fc96e2aae2767
phuocng <me@phuoc.ng>
2019-06-08 18:43:52 +0700

    Add greaterThan validator page

---
+++ src/routes/guide/validators/greater-than/basic/Bootstrap.svelte
@@ -0,0 +1,57 @@
+<BootstrapLayout onLoaded={onLoaded}>
+    <form id="demoForm" method="POST">
+        <div class="form-group row">
+            <label class="col-sm-5 col-form-label">Your age (18+ required)</label>
+            <div class="col-sm-3">
+                <input type="text" class="form-control" name="age" />
+            </div>
+        </div>
+    </form>
+</BootstrapLayout>
+
+<script>
+import { onDestroy } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Bootstrap from 'formvalidation/dist/es6/plugins/Bootstrap';
+
+import sampleCode from './Bootstrap.programmatic';
+import BootstrapLayout from '../../../../../components/demo/BootstrapLayout.svelte';
+
+let fv;
+
+const onLoaded = () => {
+    fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            age: {
+                validators: {
+                    greaterThan: {
+                        message: 'The value must be greater than or equal to 18',
+                        min: 18,
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            bootstrap: new Bootstrap(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh'
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/validators/greater-than/basic/Bootstrap',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+};
+
+onDestroy(() => {
+    fv && fv.destroy();
+});
+</script>
