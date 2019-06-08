c5b53080e6e6d1a6951a3db442d49c9478f810f9
phuocng <me@phuoc.ng>
2019-06-08 17:38:52 +0700

    Add meid validator page

---
+++ src/routes/guide/validators/meid/basic/Bootstrap.svelte
@@ -0,0 +1,79 @@
+<BootstrapLayout onLoaded={onLoaded}>
+    <ReceiveMessage channel="SAMPLE_FIELD_VALUE" sender="/guide/validators/meid/basic" on:received={receive}>
+        <form id="demoForm" method="POST">
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">MEID</label>
+                <div class="col-sm-5">
+                    <input type="text" bind:this={inputEle} class="form-control" name="meid" />
+                </div>
+            </div>
+        </form>
+    </ReceiveMessage>
+</BootstrapLayout>
+
+<script>
+import { onDestroy } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Bootstrap from 'formvalidation/dist/es6/plugins/Bootstrap';
+import meid from 'formvalidation/dist/es6/validators/meid';
+
+import sampleCode from './Bootstrap.programmatic';
+import BootstrapLayout from '../../../../../components/demo/BootstrapLayout.svelte';
+import ReceiveMessage from '../../../../../components/ReceiveMessage.svelte';
+
+let fv;
+let inputEle;
+
+const receive = (e) => {
+    const v = e.detail.data;
+    inputEle.value = v;
+
+    if (fv) {
+        fv.revalidateField('meid').then((result) => {
+            window.parent.postMessage({
+                channel: 'DEMO_VALIDATE_RESULT',
+                sender: '/guide/validators/meid/basic',
+                data: {
+                    input: v,
+                    output: result
+                },
+            }, '*');
+        });
+    }
+};
+
+const onLoaded = () => {
+    fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            meid: {
+                validators: {
+                    meid: {
+                        message: 'The value is not valid MEID'
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            bootstrap: new Bootstrap(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh'
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/validators/meid/basic/Bootstrap',
+                sampleCode: sampleCode,
+            }),
+        },
+    }).registerValidator('meid', meid);
+};
+
+onDestroy(() => {
+    fv && fv.destroy();
+});
+</script>
