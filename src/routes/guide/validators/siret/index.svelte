11242b67a63c8dac453101cf63c94036e4bc1fbb
phuocng <me@phuoc.ng>
2019-06-08 17:53:08 +0700

    Add siret validator page

---
+++ src/routes/guide/validators/siret/index.svelte
@@ -0,0 +1,67 @@
+<svelte:head>
+	<title>FormValidation • siret validator</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">siret validator</h1>
+    <h2 class="f4 fw4 tc">Validate a <a href="https://en.wikipedia.org/wiki/SIRET_code" class="blue dim link">Siret number</a></h2>
+
+    <section class="mv5">
+        <Heading>Options</Heading>
+
+        <h3>Using with form field</h3>
+        <p class="lh-copy i">The HTML attributes are used to set the validator options via the <a href="/guide/plugins/declarative/" class="blue dim link">Declarative</a> plugin</p>
+        <table class="collapse ba br2 b--black-10 pv2 ph3 w-100">
+            <tr class="striped--light-gray">
+                <th class="pv2 ph3 tl f6 fw6 ttu">Name</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">HTML attribute</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">Type</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">Description</th>
+            </tr>
+            <tr class="striped--light-gray">
+                <td class="pv2 ph3"><code>message</code></td>
+                <td class="pv2 ph3"><code>data-fv-siret___message</code></td>
+                <td class="pv2 ph3">String</td>
+                <td class="pv2 ph3">The error message</td>
+            </tr>
+        </table>
+
+        <h3>Using with ES6 module</h3>
+<SampleCode lang="javascript" code={`
+// You might need to change the importing path
+import siret from 'formvalidation/dist/es6/validators/siret';
+
+const result = siret().validate({
+    value: ...,
+    options: {
+        message: ...,
+    },
+});
+/*
+result is an object of
+{
+    valid: true or false,
+    message: The error message
+}
+*/
+`} />
+    </section>
+
+    <RelatedValidators validators={['siren']} />
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/validators/siren/">siren validator</PrevButton>
+            <NextButton href="/guide/validators/step/">step validator</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import Heading from '../../../../components/Heading.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import RelatedValidators from '../../../../components/RelatedValidators.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+</script>
