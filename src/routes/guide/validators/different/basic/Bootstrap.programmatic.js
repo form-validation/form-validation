e1e123faa9485630c96c3173d238dada0dcbb242
phuocng <me@phuoc.ng>
2019-06-08 15:31:23 +0700

    Add different validator page

---
+++ src/routes/guide/validators/different/basic/Bootstrap.programmatic.js
@@ -0,0 +1,84 @@
+export default `<!doctype html>
+<html>
+<head>
+    <link-tag rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link-tag rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link-tag rel="stylesheet" href="/vendors/formvalidation/dist/css/formValidation.min.css">
+</head>
+<body>
+    <form id="demoForm" method="POST">
+        <div class="form-group row">
+            <label class="col-sm-3 col-form-label">Username</label>
+            <div class="col-sm-5">
+                <input type="text" class="form-control" name="username" />
+            </div>
+        </div>
+        
+        <div class="form-group row">
+            <label class="col-sm-3 col-form-label">Password</label>
+            <div class="col-sm-5">
+                <input type="password" class="form-control" name="password" />
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-sm-9 offset-sm-3">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </form>
+
+<script-tag src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.3/es6-shim.min.js"></script-tag>
+<script-tag src="/vendors/formvalidation/dist/js/FormValidation.min.js"></script-tag>
+<script-tag src="/vendors/formvalidation/dist/js/plugins/Bootstrap.min.js"></script-tag>
+
+<script-tag>
+document.addEventListener('DOMContentLoaded', function(e) {
+    const form = document.getElementById('demoForm');
+    const fv = FormValidation.formValidation(
+        form,
+        {
+            fields: {
+                username: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The username is required'
+                        }
+                    }
+                },
+                password: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The password is required'
+                        },
+                        different: {
+                            compare: function() {
+                                return form.querySelector('[name="username"]').value;
+                            },
+                            message: 'The username and password cannot be the same as each other'
+                        }
+                    }
+                },
+            },
+            plugins: {
+                trigger: new FormValidation.plugins.Trigger(),
+                bootstrap: new FormValidation.plugins.Bootstrap(),
+                submitButton: new FormValidation.plugins.SubmitButton(),
+                icon: new FormValidation.plugins.Icon({
+                    valid: 'fa fa-check',
+                    invalid: 'fa fa-times',
+                    validating: 'fa fa-refresh'
+                }),
+            },
+        }
+    );
+
+    // Revalidate the password when changing the username
+    form.querySelector('[name="username"]').addEventListener('input', function() {
+        fv.revalidateField('password');
+    });
+});
+</script-tag>
+</body>
+</html>
+`;
