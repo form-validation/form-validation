3c69db881608be8d5a6943e4f4eae0fe1f74f22f
phuocng <me@phuoc.ng>
2019-06-09 18:35:06 +0700

    Add stringCase validator page

---
+++ src/routes/guide/validators/string-case/basic/Tachyons.svelte
@@ -0,0 +1,57 @@
+<TachyonsLayout>
+    <form id="demoForm" method="post">
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">Credit card holder</div>
+                <div class="fl w-50">
+                    <input type="text" name="name" class="input-reset ba b--black-20 pa2 mb2 db w-100" />
+                </div>
+            </div>
+        </div>
+    </form>
+</TachyonsLayout>
+
+<script>
+import { onMount } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Tachyons from 'formvalidation/dist/es6/plugins/Tachyons';
+
+import sampleCode from './Tachyons.programmatic';
+import TachyonsLayout from '../../../../../components/demo/TachyonsLayout.svelte';
+
+onMount(() => {
+    const fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            name: {
+                validators: {
+                    stringCase: {
+                        message: 'The card holder must be in uppercase',
+                        'case': 'upper'
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            tachyons: new Tachyons(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh'
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/validators/string-case/basic/Tachyons',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+
+    return () => {
+        fv.destroy();
+    };
+});
+</script>
---
+++ src/routes/guide/validators/string-case/index.svelte
@@ -0,0 +1,104 @@
+<svelte:head>
+	<title>FormValidation • stringCase validator</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">stringCase validator</h1>
+    <h2 class="f4 fw4 tc">Check if a string is a lower or upper case one</h2>
+
+    <section class="mv5">
+        <Heading>Options</Heading>
+
+        <h3>Using with form field</h3>
+        <p class="lh-copy i">The HTML attributes are used to set the validator options via the <a href="/guide/plugins/declarative/" class="blue dim link">Declarative</a> plugin</p>
+        <table class="collapse ba br2 b--black-10 pv2 ph3 w-100">
+            <tr class="striped--light-gray">
+                <th class="pv2 ph3 tl f6 fw6 ttu">Name</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">HTML attribute</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">Type</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">Description</th>
+            </tr>
+            <tr class="striped--light-gray">
+                <td class="pv2 ph3"><code>case</code></td>
+                <td class="pv2 ph3"><code>data-fv-string-case___case</code></td>
+                <td class="pv2 ph3">String</td>
+                <td class="pv2 ph3">Can be <code>lower</code> (the default value) or <code>upper</code></td>
+            </tr>
+            <tr class="striped--light-gray">
+                <td class="pv2 ph3"><code>message</code></td>
+                <td class="pv2 ph3"><code>data-fv-string-case___message</code></td>
+                <td class="pv2 ph3">String</td>
+                <td class="pv2 ph3">The error message</td>
+            </tr>
+        </table>
+
+        <h3>Using with ES6 module</h3>
+<SampleCode lang="javascript" code={`
+// You might need to change the importing path
+import stringCase from 'formvalidation/dist/es6/validators/stringCase';
+
+const result = stringCase().validate({
+    value: ...,
+    options: {
+        'case': ...,
+        message: ...,
+    },
+});
+// result is an object of
+// {
+//      valid: true or false,
+//      message: The error message     
+// }
+`} />
+    </section>
+
+    <section class="mv5">
+        <Heading>Basic Example</Heading>
+        <p class="lh-copy">The following form asks user to enter a credit card holder in uppercase:</p>
+        <Demo prefix="/guide/validators/string-case/basic" frameworks={['Bootstrap', 'Tachyons']} />
+    </section>
+
+    <section class="mv5">
+        <Heading>ES6 Module Example</Heading>
+
+        <p class="lh-copy">The following snippet shows how to use the stringCase validator with ES6 module:</p>
+<SampleCode lang="javascript" code={`
+// You might need to change the importing path
+import stringCase from 'formvalidation/dist/es6/validators/stringCase';
+
+const res1 = stringCase().validate({
+    value: 'JOHN SMITH',
+    options: {
+        'case': 'upper',
+        message: 'The name must be in must be in uppercase',
+    },
+});
+// res1.valid === true
+
+const res2 = stringCase().validate({
+    value: 'John Smith',
+    options: {
+        'case': 'lower',
+        message: 'The name must be in must be in lowercase',
+    },
+});
+// res2.valid === false
+`} />
+    </section>
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/validators/step/">step validator</PrevButton>
+            <NextButton href="/guide/validators/string-length/">stringLength validator</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import Demo from '../../../../components/Demo.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import Heading from '../../../../components/Heading.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+</script>
