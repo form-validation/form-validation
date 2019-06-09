9ce3c0e7a5e4c844746fe46f05b0e52ba45844aa
phuocng <me@phuoc.ng>
2019-06-09 19:29:04 +0700

    Add notEmpty validator

---
+++ src/routes/guide/validators/not-empty/html5/Tachyons.svelte
@@ -0,0 +1,63 @@
+<TachyonsLayout>
+    <form id="demoForm" method="post">
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">User name</div>
+                <div class="fl w-50">
+                    <input type="text" name="username" class="input-reset ba b--black-20 pa2 mb2 db w-100"
+                        required
+                        data-fv-not-empty___message="The username is required" />
+                </div>
+            </div>
+        </div>
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">Password</div>
+                <div class="fl w-50">
+                    <input type="password" name="password" class="input-reset ba b--black-20 pa2 mb2 db w-100"
+                        required
+                        data-fv-not-empty___message="The password is required" />
+                </div>
+            </div>
+        </div>
+    </form>
+</TachyonsLayout>
+
+<script>
+import { onMount } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import Declarative from 'formvalidation/dist/es6/plugins/Declarative';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Tachyons from 'formvalidation/dist/es6/plugins/Tachyons';
+
+import sampleCode from './Tachyons.programmatic';
+import TachyonsLayout from '../../../../../components/demo/TachyonsLayout.svelte';
+
+onMount(() => {
+    const fv = formValidation(document.getElementById('demoForm'), {
+        plugins: {
+            declarative: new Declarative({
+                html5Input: true,
+            }),
+            trigger: new Trigger(),
+            tachyons: new Tachyons(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh',
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/validators/not-empty/html5/Tachyons',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+
+    return () => {
+        fv.destroy();
+    };
+});
+</script>
---
+++ src/routes/guide/validators/not-empty/index.svelte
@@ -0,0 +1,128 @@
+<svelte:head>
+	<title>FormValidation • notEmpty validator</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">notEmpty validator</h1>
+    <h2 class="f4 fw4 tc">Check if the value an is empty string</h2>
+
+    <section class="mv5">
+        <Heading>Options</Heading>
+
+        <h3>Using with form field</h3>
+        <p class="lh-copy i">The HTML attributes are used to set the validator options via the <a href="/guide/plugins/declarative/" class="blue dim link">Declarative</a> plugin</p>
+        <table class="collapse ba br2 b--black-10 pv2 ph3 w-100 mb4">
+            <tr class="striped--light-gray">
+                <th class="pv2 ph3 tl f6 fw6 ttu">Name</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">HTML attribute</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">Type</th>
+                <th class="pv2 ph3 tl f6 fw6 ttu">Description</th>
+            </tr>
+            <tr class="striped--light-gray">
+                <td class="pv2 ph3"><code>message</code></td>
+                <td class="pv2 ph3"><code>data-fv-not-empty___message</code></td>
+                <td class="pv2 ph3">String</td>
+                <td class="pv2 ph3">The error message</td>
+            </tr>
+        </table>
+
+        <h3>Use with select element</h3>
+        <p class="lh-copy">If you want a select element to be required, you have to set <code>value=""</code> for the option which is treated as empty one:</p>
+<SampleCode lang="html" code={`
+<select name="gender">
+    <option value="">Select the gender</option>
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+    <option value="other">Other</option>
+</select>
+`} />
+
+        <h3>Using with ES6 module</h3>
+<SampleCode lang="javascript" code={`
+// You might need to change the importing path
+import notEmpty from 'formvalidation/dist/es6/validators/notEmpty';
+
+const result = notEmpty().validate({
+    value: ...,
+    options: {
+        message: ...,
+    },
+});
+/*
+result is an object of
+{
+    valid: true or false,
+    message: The error message
+}
+*/
+`} />
+        <Tip>The <a href="/guide/plugins/mandatory-icon/" class="blue dim link">MandatoryIcon</a> plugin is useful when you want to display the mandatory icon for required fields</Tip>
+    </section>
+
+    <section class="mv5">
+        <Heading>Basic example</Heading>
+        <p class="lh-copy">In the following form, user is asked to enter the full name.</p>
+        <Demo prefix="/guide/validators/not-empty/basic" frameworks={['Bootstrap', 'Tachyons']} />
+    </section>
+
+    <section class="mv5">
+        <Heading>HTML5 example</Heading>
+        <p class="lh-copy">When the <a href="/guide/plugins/declarative/" class="blue dim link">Declarative</a> plugin is used, the notEmpty validator will be turned on automatically if the input uses HTML 5 <code>required</code> attribute.</p>
+        <Demo prefix="/guide/validators/not-empty/html5" frameworks={['Tachyons']} />
+    </section>
+
+    <section class="mv5">
+        <Heading>ES6 Module Example</Heading>
+
+        <p class="lh-copy">The following snippet shows how to use the notEmpty validator with ES6 module:</p>
+<SampleCode lang="javascript" code={`
+// You might need to change the importing path
+import notEmpty from 'formvalidation/dist/es6/validators/notEmpty';
+
+const res1 = notEmpty().validate({
+    value: 'John Smith',
+    options: {
+        message: 'The name is required',
+    },
+});
+// res1.valid === true
+
+const res2 = notEmpty().validate({
+    value: '',
+    options: {
+        message: 'The name is required',
+    },
+});
+// res2.valid === false
+
+const res3 = notEmpty().validate({
+    value: '   ',
+    options: {
+        message: 'The name is required',
+    },
+});
+// res3.valid === false
+`} />
+    </section>
+
+    <RelatedValidators validators={['stringLength']} />
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/validators/meid/">meid validator</PrevButton>
+            <NextButton href="/guide/validators/numeric/">numeric validator</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import Demo from '../../../../components/Demo.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import Heading from '../../../../components/Heading.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import RelatedValidators from '../../../../components/RelatedValidators.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+import SampleData from '../../../../components/SampleData.svelte';
+import Tip from '../../../../components/Tip.svelte';
+</script>
