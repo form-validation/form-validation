fa0d788ae6d703d2b12e7bff5b39edd6eb5316b5
phuocng <me@phuoc.ng>
2019-06-09 21:20:49 +0700

    Add Sequence plugin page

---
+++ src/routes/guide/plugins/sequence/basic/Tachyons.svelte
@@ -0,0 +1,77 @@
+<TachyonsLayout>
+    <form id="demoForm" method="POST">
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">Username</div>
+                <div class="fl w-50">
+                    <input type="text" class="input-reset ba b--black-20 pa2 mb2 db w-100" name="username" />
+                </div>
+            </div>
+        </div>
+    </form>
+</TachyonsLayout>
+
+<script>
+
+import { onMount } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Sequence from 'formvalidation/dist/es6/plugins/Sequence';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Tachyons from 'formvalidation/dist/es6/plugins/Tachyons';
+
+import sampleCode from './Tachyons.programmatic';
+import TachyonsLayout from '../../../../../components/demo/TachyonsLayout.svelte';
+
+onMount(() => {
+    const fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required'
+                    },
+                    callback: {
+                        message: 'This username is not allowed',
+                        callback: (input) => !input.value.startsWith('admin'),
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: 'The username must be more than 6 and less than 30 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_]+$/,
+                        message: 'The username can only consist of alphabetical, number and underscore'
+                    },
+                    remote: {
+                        message: 'The username is already taken',
+                        method: 'GET',
+                        url: 'http://www.mocky.io/v2/5b40b1902f0000810079e0de',
+                    },
+                },
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            tachyons: new Tachyons(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh',
+            }),
+            sequence: new Sequence(),
+            demoFrame: new DemoFrame({
+                sender: '/guide/plugins/sequence/basic/Tachyons',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+
+    return () => {
+        fv.destroy();
+    };
+});
+</script>
