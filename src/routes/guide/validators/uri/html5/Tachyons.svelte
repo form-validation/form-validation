421ef6a55f9f2b62ce481bf10258dfeeb2b7b006
phuocng <me@phuoc.ng>
2019-06-09 20:21:55 +0700

    Add uri validator

---
+++ src/routes/guide/validators/uri/html5/Tachyons.svelte
@@ -0,0 +1,53 @@
+<TachyonsLayout>
+    <form id="demoForm" method="post">
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">Your website</div>
+                <div class="fl w-50">
+                    <input name="website" class="input-reset ba b--black-20 pa2 mb2 db w-100"
+                        type="url"
+                        data-fv-uri___message="The website address is not valid" />
+                </div>
+            </div>
+        </div>
+    </form>
+</TachyonsLayout>
+
+<script>
+import { onMount } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import Declarative from 'formvalidation/dist/es6/plugins/Declarative';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Tachyons from 'formvalidation/dist/es6/plugins/Tachyons';
+
+import sampleCode from './Tachyons.programmatic';
+import TachyonsLayout from '../../../../../components/demo/TachyonsLayout.svelte';
+
+onMount(() => {
+    const fv = formValidation(document.getElementById('demoForm'), {
+        plugins: {
+            declarative: new Declarative({
+                html5Input: true,
+            }),
+            trigger: new Trigger(),
+            tachyons: new Tachyons(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh',
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/validators/uri/html5/Tachyons',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+
+    return () => {
+        fv.destroy();
+    };
+});
+</script>
