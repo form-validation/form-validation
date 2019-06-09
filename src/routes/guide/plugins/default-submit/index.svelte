68acf0401edcd83042d03f025640982ce89f7f75
phuocng <me@phuoc.ng>
2019-06-09 21:02:09 +0700

    Add DefaultSubmit plugin page

---
+++ src/routes/guide/plugins/default-submit/index.svelte
@@ -0,0 +1,74 @@
+<svelte:head>
+	<title>FormValidation • DefaultSubmit plugin</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">DefaultSubmit plugin</h1>
+    <h2 class="f4 fw4 tc">Submit the form if all fields are valid after validating</h2>
+
+    <section class="mv5">
+        <Heading>Usage</Heading>
+        <p class="lh-copy">The following piece of code is the starting point to use the DefaultSubmit plugin:</p>
+<SampleCode lang="html" code={`
+<html>
+<head>
+    <link-tag rel="stylesheet" href="/vendors/formvalidation/dist/css/formValidation.min.css">
+</head>
+<body>
+    <form id="demoForm" method="POST">
+        ...
+        <!-- Do NOT use name="submit" or id="submit" for the Submit button -->
+        <button type="submit">Submit</button>
+    </form>
+
+<script-tag src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.3/es6-shim.min.js"></script-tag>
+<script-tag src="/vendors/formvalidation/dist/js/FormValidation.min.js"></script-tag>
+
+<script-tag>
+document.addEventListener('DOMContentLoaded', function(e) {
+    FormValidation.formValidation(
+        document.getElementById('demoForm'),
+        {
+            fields: {
+                ...
+            },
+            plugins: {
+                defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+                ...
+            },
+        }
+    );
+});
+</script-tag>
+</body>
+</html>
+`} />
+        <p class="lh-copy">The sample code above assumes that the FormValidation files are placed inside the <code>vendors</code> directory. You might need to change the path depending on where you place them on the server.</p>
+        <p class="lh-copy">Instead of submitting the form, you can use an <a href="/guide/events/core.form.valid/#example-using-ajax-to-submit-the-form" class="blue dim link">Ajax request</a> to send the form data to the server.</p>
+        <Tip>If you want the form to be validated automatically when pressing its Submit button, use the <a href="/guide/plugins/submit-button/" class="blue dim link">SubmitButton</a> plugin</Tip>
+    </section>
+
+    <section class="mv5">
+        <Heading>Changelog</Heading>
+        <ul class="pa0 ma0 ml3 lh-copy">
+            <li>v1.0.0: First release</li>
+        </ul>
+    </section>
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/plugins/declarative/">Declarative plugin</PrevButton>
+            <NextButton href="/guide/plugins/excluded/">Excluded plugin</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import Demo from '../../../../components/Demo.svelte';
+import Heading from '../../../../components/Heading.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+import Tip from '../../../../components/Tip.svelte';
+</script>
