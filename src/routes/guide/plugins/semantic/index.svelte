2580caae22fa0b4890d5e1f80cfbdacf23f9f1c8
phuocng <me@phuoc.ng>
2019-06-10 21:07:06 +0700

    Add semantic UI plugin page

---
+++ src/routes/guide/plugins/semantic/index.svelte
@@ -0,0 +1,134 @@
+<svelte:head>
+	<title>FormValidation • Semantic plugin</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">Semantic plugin</h1>
+    <h2 class="f4 fw4 tc lh-copy">Integrate with <a href="https://semantic-ui.com/" class="blue dim link">Semantic UI</a> framework. Support Semantic UI v2.4.1</h2>
+
+    <section class="mv5">
+        <Heading>Usage</Heading>
+        <p class="lh-copy">The following piece of code is the starting point to validate the form made in Semantic UI:</p>
+<SampleCode lang="html" code={`
+<html>
+<head>
+    <link-tag rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+    <link-tag rel="stylesheet" href="/vendors/formvalidation/dist/css/formValidation.min.css">
+</head>
+<body>
+    <form id="demoForm" method="POST">
+        ...
+    </form>
+
+<script-tag src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.3/es6-shim.min.js"></script-tag>    
+<script-tag src="/vendors/formvalidation/dist/js/FormValidation.min.js"></script-tag>
+<script-tag src="/vendors/formvalidation/dist/js/plugins/Semantic.min.js"></script-tag>
+
+<script-tag>
+document.addEventListener('DOMContentLoaded', function(e) {
+    FormValidation.formValidation(
+        document.getElementById('demoForm'),
+        {
+            fields: {
+                ...
+            },
+            plugins: {
+                semantic: new FormValidation.plugins.Semantic(),
+                ...
+            },
+        }
+    );
+});
+</script-tag>
+</body>
+</html>
+`} />
+        <p class="lh-copy">There are some important things here:</p>
+        <ul class="ma0 pl3 lh-copy">
+            <li>The sample code above assumes that the FormValidation files are placed inside the <code>vendors</code> directory. You might need to change the path depending on where you place them on the server.</li>
+            <li><code>Semantic.min.js</code> is the plugin provided by FormValidation. It is NOT the same as <code>Semantic(.min).js</code> file provided by the Semantic UI framework.</li>
+        </ul>
+        <p class="lh-copy">The next sections list out some examples of various forms made with Semantic UI.</p>
+    </section>
+
+    <section class="mv5">
+        <Heading>Horizontal form</Heading>
+        <Demo prefix="/guide/plugins/semantic/horizontal-form" frameworks={['Semantic']} />
+    </section>
+
+    <section class="mv5">
+        <Heading>Stacked form</Heading>
+        <p class="lh-copy">In order to add the correct class for error message and the field element when it is a valid or invalid, we need to specify the CSS selector of the field container.</p>
+        <p class="lh-copy">By default, the Semantic plugin will look for the <code>.fields</code> element. But in the stacked form, evey field will be placed inside a <code>.field</code> container.</p>
+        <p class="lh-copy">We need to use the <code>rowSelector</code> option to help the plugin determine the field containers as following:</p>
+        <div class="mb4">
+            <SampleCode lang="javascript" code={`
+semantic: new FormValidation.plugins.Semantic({
+    rowSelector: function(field, ele) {
+        // field is the field name
+        // ele is the field element
+        return '.field';
+    }
+
+    // Or you can just simply pass it as a string:
+    // rowSelector: '.field',
+}),
+`} />    
+        </div>      
+        <Demo prefix="/guide/plugins/semantic/stacked-form" frameworks={['Semantic']} />
+    </section>
+
+    <section class="mv5">
+        <Heading>Multiple fields on the same row</Heading>
+        <p class="lh-copy">Due to the same reason mentioned in the <span class="i">Stacked form</span> section above, the <code>rowSelector</code> option will be used to determine the field containers.</p>
+        <p class="lh-copy">In the following example, the <code>firstName</code> and <code>lastName</code> fields are placed inside <code>.five.field</code> containers. Meanwhile, the <code>city</code>, <code>state</code> and <code>zipcode</code> fields can be found inside the <code>.four.field</code> containers.</p>
+        <div class="mb4">
+            <SampleCode lang="javascript" code={`
+semantic: new FormValidation.plugins.Semantic({
+    rowSelector: function(field, ele) {
+        // field is the field name
+        // ele is the field element
+        switch (field) {
+            case 'firstName':
+            case 'lastName':
+                return '.five.field';
+            
+            case 'city':
+            case 'state':
+            case 'zipcode':
+                return '.four.field';
+            
+            default:
+                return '.fields';
+        }
+    }
+}),
+`} />        
+        </div>
+        <Demo prefix="/guide/plugins/semantic/multiple-fields" frameworks={['Semantic']} />
+    </section>
+
+    <section class="mv5">
+        <Heading>Changelog</Heading>
+        <ul class="pa0 ma0 ml3 lh-copy">
+            <li>v1.2.0: Supported Semantic UI v2.3.3</li>
+            <li>v1.0.0: First release</li>
+        </ul>
+    </section>
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/plugins/recaptcha/">Recaptcha plugin</PrevButton>
+            <NextButton href="/guide/plugins/sequence/">Sequence plugin</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import Demo from '../../../../components/Demo.svelte';
+import Heading from '../../../../components/Heading.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+</script>
---
+++ src/routes/guide/plugins/semantic/multiple-fields/Semantic.svelte
@@ -0,0 +1,164 @@
+<style>
+:global(.ui.form .fields) {
+    margin: 0 -.5em 1em;
+}
+:global(.ui.form:not(.unstackable) .fields:not(.unstackable)>.four.wide.field) {
+    width: 25%!important;
+}
+:global(.ui.form:not(.unstackable) .fields:not(.unstackable)>.five.wide.field) {
+    width: 31.25%!important;
+}
+</style>
+
+<SemanticLayout onLoaded={onLoaded}>
+    <form id="demoForm" method="POST" class="ui form">
+        <div class="fields">
+            <label class="four wide field">Full name</label>
+            <div class="five wide field">
+                <!--
+                When using feedback icon, the input must place inside 'ui input icon' element
+                <div class="ui input icon">
+                    <input type="text" />
+                </div>
+                See http://semantic-ui.com/elements/input.html#icon
+                -->
+                <div class="ui input icon">
+                    <input type="text" name="firstName" placeholder="First name" />
+                </div>
+            </div>
+            <div class="five wide field">
+                <div class="ui input icon">
+                    <input type="text" name="lastName" placeholder="Last name" />
+                </div>
+            </div>
+        </div>
+    
+        <div class="fields">
+            <label class="four wide field">Address</label>
+            <div class="four wide field">
+                <div class="ui input icon">
+                    <input type="text" name="city" placeholder="City" />
+                </div>
+            </div>
+            <div class="four wide field">
+                <div class="ui input icon">
+                    <input type="text" name="state" placeholder="State" />
+                </div>
+            </div>
+            <div class="four wide field">
+                <div class="ui input icon">
+                    <input type="text" name="zipcode" placeholder="Zipcode" />
+                </div>
+            </div>
+        </div>
+    
+        <div class="fields">
+            <label class="four wide field"></label>
+            <div class="four wide field">
+                <button type="submit" class="ui primary button">Submit</button>
+            </div>
+        </div>
+    </form>
+</SemanticLayout>
+
+<script>
+import { onDestroy } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Semantic from 'formvalidation/dist/es6/plugins/Semantic';
+import SubmitButton from 'formvalidation/dist/es6/plugins/SubmitButton';
+
+import sampleCode from './Semantic.programmatic';
+import SemanticLayout from '../../../../../components/demo/SemanticLayout.svelte';
+
+let fv;
+
+const onLoaded = () => {
+    fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            firstName: {
+                validators: {
+                    notEmpty: {
+                        message: 'The first name is required'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z\s]+$/,
+                        message: 'The first name can only consist of alphabetical and space'
+                    }
+                }
+            },
+            lastName: {
+                validators: {
+                    notEmpty: {
+                        message: 'The last name is required'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z\s]+$/,
+                        message: 'The last name can only consist of alphabetical and space'
+                    }
+                }
+            },
+            city: {
+                validators: {
+                    notEmpty: {
+                        message: 'The city is required'
+                    }
+                }
+            },
+            state: {
+                validators: {
+                    notEmpty: {
+                        message: 'The state is required'
+                    }
+                }
+            },
+            zipcode: {
+                validators: {
+                    notEmpty: {
+                        message: 'The zipcode is required'
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            semantic: new Semantic({
+                rowSelector: (field, ele) => {
+                    // field is the field name
+                    // ele is the field element
+                    switch (field) {
+                        case 'firstName':
+                        case 'lastName':
+                            return '.five.field';
+                        
+                        case 'city':
+                        case 'state':
+                        case 'zipcode':
+                            return '.four.field';
+                        
+                        default:
+                            return '.fields';
+                    }
+                }
+            }),
+            submitButton: new SubmitButton(),
+            icon: new Icon({
+                valid: 'checkmark icon',
+                invalid: 'remove icon',
+                validating: 'refresh icon'
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/plugins/semantic/multiple-fields/Semantic',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+};
+
+onDestroy(() => {
+    fv && fv.destroy();
+});
+</script>
