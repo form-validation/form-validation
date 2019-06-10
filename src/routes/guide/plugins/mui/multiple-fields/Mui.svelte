3c5e38d1becbce72b242001b38ea32d80f3fa4e8
phuocng <me@phuoc.ng>
2019-06-10 20:57:06 +0700

    Add Mui plugin page

---
+++ src/routes/guide/plugins/mui/multiple-fields/Mui.svelte
@@ -0,0 +1,151 @@
+<style>
+:global(.mui-col-md-6) {
+    float: left;
+    width: 50%;
+}
+
+:global(.mui-col-md-4) {
+    float: left;
+    width: 33.33333%;
+}
+</style>
+
+<MuiLayout onLoaded={onLoaded}>
+    <div class="mui-container-fluid">
+        <form id="demoForm" method="POST">
+            <div class="mui-row">
+                <div class="mui-col-md-6 mui-textfield">
+                    <input type="text" name="firstName" />
+                    <label>First name</label>
+                </div>
+                <div class="mui-col-md-6 mui-textfield">
+                    <input type="text" name="lastName" />
+                    <label>Last name</label>
+                </div>
+            </div>
+
+            <div class="mui-row">
+                <div class="mui-col-md-4 mui-textfield">
+                    <input type="text" name="city" />
+                    <label>City</label>
+                </div>
+                <div class="mui-col-md-4 mui-textfield">
+                    <input type="text" name="state" />
+                    <label>State</label>
+                </div>
+                <div class="mui-col-md-4 mui-textfield">
+                    <input type="text" name="zipcode" />
+                    <label>Zipcode</label>
+                </div>
+            </div>
+        
+            <div class="mui-row">
+                <div class="mui-col-md-12">
+                    <button type="submit" class="mui-btn mui-btn--primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</MuiLayout>
+
+<script>
+import { onDestroy } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Mui from 'formvalidation/dist/es6/plugins/Mui';
+import SubmitButton from 'formvalidation/dist/es6/plugins/SubmitButton';
+
+import sampleCode from './Mui.programmatic';
+import MuiLayout from '../../../../../components/demo/MuiLayout.svelte';
+
+let fv;
+
+const onLoaded = () => {
+    fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            firstName: {
+                validators: {
+                    notEmpty: {
+                        message: 'The first name is required'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z\s]+$/,
+                        message: 'The first name can only consist of alphabetical and space'
+                    }
+                }
+            },
+            lastName: {
+                validators: {
+                    notEmpty: {
+                        message: 'The last name is required'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z\s]+$/,
+                        message: 'The last name can only consist of alphabetical and space'
+                    }
+                }
+            },
+            city: {
+                validators: {
+                    notEmpty: {
+                        message: 'The city is required'
+                    }
+                }
+            },
+            state: {
+                validators: {
+                    notEmpty: {
+                        message: 'The state is required'
+                    }
+                }
+            },
+            zipcode: {
+                validators: {
+                    notEmpty: {
+                        message: 'The zipcode is required'
+                    }
+                }
+            },
+        },
+        plugins: {
+            trigger: new Trigger(),
+            mui: new Mui({
+                rowSelector: (field, ele) => {
+                    // field is the field name
+                    // ele is the field element
+                    switch (field) {
+                        case 'firstName':
+                        case 'lastName':
+                            return '.mui-col-md-6';
+
+                        case 'city':
+                        case 'state':
+                        case 'zipcode':
+                            return '.mui-col-md-4';
+                        
+                        default:
+                            return '.mui-row';
+                    }
+                }
+            }),
+            submitButton: new SubmitButton(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh',
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/plugins/mui/multiple-fields/Mui',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+};
+
+onDestroy(() => {
+    fv && fv.destroy();
+});
+</script>
---
+++ src/routes/guide/plugins/mui/stacked-form/Mui.programmatic.js
@@ -0,0 +1,153 @@
+export default `<!doctype html>
+<html>
+<head>
+    <link-tag rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link-tag rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/muicss/0.9.41/css/mui.min.css">
+    <link-tag rel="stylesheet" href="/vendors/formvalidation/dist/css/formValidation.min.css">
+</head>
+<body>
+    <div class="mui-container-fluid">
+        <form id="demoForm" method="POST">
+            <div class="mui-row">
+                <div class="mui-col-md-12 mui-textfield">
+                    <input type="text" name="name" />
+                    <label>Product name</label>
+                </div>
+            </div>
+
+            <div class="mui-row">
+                <div class="mui-col-md-12 mui-textfield">
+                    <input type="text" name="price" />
+                    <label>Price ($)</label>
+                </div>
+            </div>
+
+            <div class="mui-row">
+                <div class="mui-col-md-12">
+                    <label>Size</label>
+                </div>
+            </div>
+
+            <div class="mui-row">
+                <div class="mui-col-md-12">
+                    <div class="mui-checkbox">
+                        <label>
+                            <input type="checkbox" name="size[]" value="s" /> S
+                        </label>
+                    </div>
+                    <div class="mui-checkbox">
+                        <label>
+                            <input type="checkbox" name="size[]" value="m" /> M
+                        </label>
+                    </div>
+                    <div class="mui-checkbox">
+                        <label>
+                            <input type="checkbox" name="size[]" value="l" /> L
+                        </label>
+                    </div>
+                    <div class="mui-checkbox">
+                        <label>
+                            <input type="checkbox" name="size[]" value="xl" /> XL
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mui-row">
+                <div class="mui-col-md-12">
+                    <label>Available in store</label>
+                </div>
+            </div>
+
+            <div class="mui-row">
+                <div class="mui-col-md-12">
+                    <div class="mui-radio">
+                        <label>
+                            <input type="radio" name="availability" value="yes" /> Yes
+                        </label>
+                    </div>
+                    <div class="mui-radio">
+                        <label>
+                            <input type="radio" name="availability" value="no" /> No
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mui-row">
+                <div class="mui-col-md-12">
+                    <button type="submit" class="mui-btn mui-btn--primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+<script-tag src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.3/es6-shim.min.js"></script-tag>
+<script-tag src="https://cdnjs.cloudflare.com/ajax/libs/muicss/0.9.41/js/mui.min.js"></script>
+<script-tag src="/vendors/formvalidation/dist/js/FormValidation.min.js"></script-tag>
+<script-tag src="/vendors/formvalidation/dist/js/plugins/Mui.min.js"></script-tag>
+
+<script-tag>
+document.addEventListener('DOMContentLoaded', function(e) {
+    FormValidation.formValidation(
+        document.getElementById('demoForm'),
+        {
+            fields: {
+                name: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The name is required'
+                        },
+                        stringLength: {
+                            min: 6,
+                            max: 30,
+                            message: 'The name must be more than 6 and less than 30 characters long'
+                        },
+                        regexp: {
+                            regexp: /^[a-zA-Z0-9_]+$/,
+                            message: 'The name can only consist of alphabetical, number and underscore'
+                        }
+                    }
+                },
+                price: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The price is required'
+                        },
+                        numeric: {
+                            message: 'The price must be a number'
+                        }
+                    }
+                },
+                'size[]': {
+                    validators: {
+                        notEmpty: {
+                            message: 'The size is required'
+                        }
+                    }
+                },
+                availability: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The availability option is required'
+                        }
+                    }
+                },
+            },
+            plugins: {
+                trigger: new FormValidation.plugins.Trigger(),
+                mui: new FormValidation.plugins.Mui(),
+                submitButton: new FormValidation.plugins.SubmitButton(),
+                icon: new FormValidation.plugins.Icon({
+                    valid: 'fa fa-check',
+                    invalid: 'fa fa-times',
+                    validating: 'fa fa-refresh',
+                }),
+            },
+        }
+    );
+});
+</script-tag>
+</body>
+</html>
+`;
