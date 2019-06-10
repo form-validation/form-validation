2ef5f8def6b64182d42790be476f02bcf854db4f
phuocng <me@phuoc.ng>
2019-06-10 21:59:15 +0700

    Add trigger plugin page

---
+++ src/routes/guide/plugins/trigger/basic/Tachyons.svelte
@@ -0,0 +1,93 @@
+<TachyonsLayout>
+    <form id="demoForm" method="POST">
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">Title</div>
+                <div class="fl w-30">
+                    <input type="text" class="input-reset ba b--black-20 pa2 mb2 db w-100" name="title" />
+                </div>
+            </div>
+        </div>
+
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">Summary</div>
+                <div class="fl w-75">
+                    <textarea rows="5" class="input-reset ba b--black-20 pa2 mb2 db w-100" name="summary"></textarea>
+                </div>
+            </div>
+        </div>
+
+        <div class="cf mb2">
+            <div class="fl w-100">
+                <div class="fl w-25 pa2">Description</div>
+                <div class="fl w-75">
+                    <textarea rows="5" class="input-reset ba b--black-20 pa2 mb2 db w-100" name="description"></textarea>
+                </div>
+            </div>
+        </div>
+    </form>
+</TachyonsLayout>
+
+<script>
+import { onMount } from 'svelte';
+
+import formValidation from 'formvalidation/dist/es6/core/Core';
+import DemoFrame from 'formvalidation/dist/es6/plugins/DemoFrame';
+import Icon from 'formvalidation/dist/es6/plugins/Icon';
+import Trigger from 'formvalidation/dist/es6/plugins/Trigger';
+import Tachyons from 'formvalidation/dist/es6/plugins/Tachyons';
+
+import sampleCode from './Tachyons.programmatic';
+import TachyonsLayout from '../../../../../components/demo/TachyonsLayout.svelte';
+
+onMount(() => {
+    const fv = formValidation(document.getElementById('demoForm'), {
+        fields: {
+            title: {
+                validators: {
+                    notEmpty: {
+                        message: 'The title is required'
+                    }
+                }
+            },
+            summary: {
+                validators: {
+                    notEmpty: {
+                        message: 'The summary is required'
+                    }
+                }
+            },
+            description: {
+                validators: {
+                    notEmpty: {
+                        message: 'The description is required'
+                    }
+                }
+            },
+        },
+        plugins: {
+            tachyons: new Tachyons(),
+            icon: new Icon({
+                valid: 'fa fa-check',
+                invalid: 'fa fa-times',
+                validating: 'fa fa-refresh',
+            }),
+            trigger: new Trigger({
+                event: {
+                    title: 'keyup',
+                    summary: 'blur',
+                },
+            }),
+            demoFrame: new DemoFrame({
+                sender: '/guide/plugins/trigger/basic/Tachyons',
+                sampleCode: sampleCode,
+            }),
+        },
+    });
+
+    return () => {
+        fv.destroy();
+    };
+});
+</script>
