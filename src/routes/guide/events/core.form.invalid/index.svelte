ccc767be038abc910e6a6c1a5c4187719bd022cd
phuocng <me@phuoc.ng>
2019-06-11 23:57:14 +0700

    Add core.form.invalid event

---
+++ src/routes/guide/events/core.form.invalid/index.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+	<title>FormValidation • core.form.invalid event</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">core.form.invalid event</h1>
+    <h2 class="f4 fw4 tc">Triggered when the form is completely validated, and all fields are invalid</h2>
+
+    <Examples examples={[
+        'Regenerating captcha when the form is invalid',
+    ]} />
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/events/core.field.validating/">core.field.validating event</PrevButton>
+            <NextButton href="/guide/events/core.form.notvalidated/">core.form.notvalidated event</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+import Tip from '../../../../components/Tip.svelte';
+</script>
