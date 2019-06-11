58e45352847579b400fa68d296cd956677b14636
phuocng <me@phuoc.ng>
2019-06-11 22:30:00 +0700

    Add getFields() page

---
+++ src/routes/guide/api/get-fields/index.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+	<title>FormValidation • getFields() method</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">getFields()</h1>
+    <h2 class="f4 fw4 tc">Return entire <code>fields</code> option.</h2>
+
+    <section class="mv5">
+        <SampleCode lang="javascript" code={`getFields(): Object`} />
+    </section>
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/api/get-element-value/">getElementValue() method</PrevButton>
+            <NextButton href="/guide/api/get-form-element/">getFormElement() method</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import Examples from '../../../../components/Examples.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+</script>
