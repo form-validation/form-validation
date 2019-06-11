434d6a8be96c669cfa29ce285e038f6ae58e5d67
phuocng <me@phuoc.ng>
2019-06-12 00:08:38 +0700

    Add core.form.valid event page

---
+++ src/routes/guide/events/core.form.valid/index.svelte
@@ -0,0 +1,28 @@
+<svelte:head>
+	<title>FormValidation • core.form.valid event</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">core.form.valid event</h1>
+    <h2 class="f4 fw4 tc lh-copy">Triggered when the form is completely validated, and all fields are valid</h2>
+
+    <Examples examples={[
+        'Multiple steps wizard',
+        'Using Ajax to submit the form',
+    ]} />
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/events/core.form.reset/">core.form.reset event</PrevButton>
+            <NextButton href="/guide/events/core.form.validating/">core.form.validating event</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import Examples from '../../../../components/Examples.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import Tip from '../../../../components/Tip.svelte';
+</script>
