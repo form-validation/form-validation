52e3a0ccb727a9c0588f0b56dc492995496a4e60
phuocng <me@phuoc.ng>
2019-06-11 23:07:35 +0700

    Add updateFieldStatus() page

---
+++ src/routes/guide/api/update-field-status/index.svelte
@@ -0,0 +1,45 @@
+<svelte:head>
+	<title>FormValidation • updateFieldStatus() method</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">updateFieldStatus()</h1>
+    <h2 class="f4 fw4 tc">Update the field status</h2>
+
+    <section class="mv5">
+        <div class="mb3">
+            <SampleCode lang="javascript" code={`updateFieldStatus(field: String, status: String, validator: String): Core`} />
+        </div>
+        <p class="lh-copy i"><sup>*</sup> presents a required parameter</p>
+        <ul class="ma0 pl3 lh-copy">
+            <li><code>field</code> <sup>*</sup> — The field name</li>
+            <li><code>status</code> <sup>*</sup> — The new status. It can be one of the following value
+                <ul class="ma0 pl3 lh-copy">
+                    <li><code>'NotValidated'</code> or <code>FormValidation.Status.NotValidated</code>: The field is not yet validated</li>
+                    <li><code>'Valid'</code> or <code>FormValidation.Status.Valid</code>: The field is valid</li>
+                    <li><code>'Invalid'</code> or <code>FormValidation.Status.Invalid</code>: The field is invalid</li>
+                </ul>
+            </li>
+            <li><code>validator</code> — The validator name. If it isn't specified, all validators will be updated</li>
+        </ul>
+    </section>
+
+    <Examples examples={[
+        'Validating credit card expiration date',
+    ]} />
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/api/update-element-status/">updateElementStatus() method</PrevButton>
+            <NextButton href="/guide/api/update-validator-option/">updateValidatorOption() method</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import Examples from '../../../../components/Examples.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+</script>
