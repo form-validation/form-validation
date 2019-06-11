31909cfa5e5c467709d6ee6941dea440ca44b3be
phuocng <me@phuoc.ng>
2019-06-11 21:30:29 +0700

    Add Adjusting icon position example

---
+++ src/routes/guide/examples/adjusting-icon-position/Bootstrap.programmatic.js
@@ -0,0 +1,120 @@
+export default `<!doctype html>
+<html>
+<head>
+    <link-tag rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link-tag rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link-tag rel="stylesheet" href="/vendors/formvalidation/dist/css/formValidation.min.css">
+
+    <style>
+    // Customize the icon's position
+    .fv-plugins-bootstrap .form-group.inputGroupContainer .fv-plugins-icon,
+    .fv-plugins-bootstrap .form-group.selectContainer .fv-plugins-icon {
+        right: -20px;
+    }
+    </style>
+</head>
+<body>
+    <form id="demoForm" method="POST">
+        <div class="form-group row">
+            <label class="col-sm-3 col-form-label">Product name</label>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" name="name" />
+            </div>
+        </div>
+
+        <div class="form-group row inputGroupContainer">
+            <label class="col-sm-3 col-form-label">Price</label>
+            <div class="col-sm-5">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">$</span>
+                    </div>
+                    <input type="text" class="form-control" name="price" />
+                    <div class="input-group-append">
+                        <span class="input-group-text">.00</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group row selectContainer">
+            <label class="col-sm-3 col-form-label">Size</label>
+            <div class="col-sm-5">
+                <select class="form-control" name="size">
+                    <option value="">Choose a size</option>
+                    <option value="S">S</option>
+                    <option value="M">M</option>
+                    <option value="L">L</option>
+                    <option value="XL">XL</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-sm-9 offset-sm-3">
+                <button type="submit" class="btn btn-primary">Add Product</button>
+            </div>
+        </div>
+    </form>
+
+<script-tag src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.3/es6-shim.min.js"></script-tag>    
+<script-tag src="/vendors/formvalidation/dist/js/FormValidation.min.js"></script-tag>
+<script-tag src="/vendors/formvalidation/dist/js/plugins/Bootstrap.min.js"></script-tag>
+
+<script-tag>
+document.addEventListener('DOMContentLoaded', function(e) {
+    FormValidation.formValidation(
+        document.getElementById('demoForm'),
+        {
+            fields: {
+                name: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The name is required'
+                        },
+                        stringLength: {
+                            min: 6,
+                            max: 30,
+                            message: 'The name must be more than 6 and less than 30 characters long'
+                        },
+                        regexp: {
+                            regexp: /^[a-zA-Z0-9_]+$/,
+                            message: 'The name can only consist of alphabetical, number and underscore'
+                        }
+                    }
+                },
+                price: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The price is required'
+                        },
+                        numeric: {
+                            message: 'The price must be a number'
+                        }
+                    }
+                },
+                size: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The size is required'
+                        }
+                    }
+                },
+            },
+            plugins: {
+                trigger: new FormValidation.plugins.Trigger(),
+                bootstrap: new FormValidation.plugins.Bootstrap(),
+                submitButton: new FormValidation.plugins.SubmitButton(),
+                icon: new FormValidation.plugins.Icon({
+                    valid: 'fa fa-check',
+                    invalid: 'fa fa-times',
+                    validating: 'fa fa-refresh',
+                }),
+            },
+        }
+    );
+});
+</script-tag>
+</body>
+</html>
+`;
