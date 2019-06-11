128f503a06063a8b3e9203a4cbcc826c70792379
phuocng <me@phuoc.ng>
2019-06-11 22:30:54 +0700

    Add getFormElement() page

---
+++ src/routes/guide/api/get-form-element/index.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+	<title>FormValidation • getFormElement() method</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">getFormElement()</h1>
+    <h2 class="f4 fw4 tc">Return the form element.</h2>
+
+    <section class="mv5">
+        <SampleCode lang="javascript" code={`getFormElement(): HTMLElement`} />
+    </section>
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/api/get-fields/">getFields() method</PrevButton>
+            <NextButton href="/guide/api/get-plugin/">getPlugin() method</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import Examples from '../../../../components/Examples.svelte';
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+import SampleCode from '../../../../components/SampleCode.svelte';
+</script>
