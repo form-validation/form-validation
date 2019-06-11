923f72e95d6d38f7610830066945621b23cc2a35
phuocng <me@phuoc.ng>
2019-06-12 00:17:21 +0700

    Add core.form.validating event page

---
+++ src/routes/guide/events/core.form.validating/index.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+	<title>FormValidation • core.form.validating event</title>
+</svelte:head>
+
+<GuideLayout>
+    <h1 class="f3 f2-m f1-l tc">core.form.validating event</h1>
+    <h2 class="f4 fw4 tc">Triggered when starting validating the form</h2>
+
+    <section class="mv5">
+        <div class="flex">
+            <PrevButton href="/guide/events/core.form.valid/">core.form.valid event</PrevButton>
+            <NextButton href="/guide/events/core.validator.notvalidated/">core.validator.notvalidated event</NextButton>
+        </div>
+    </section>
+</GuideLayout>
+
+<script>
+import GuideLayout from '../../../../components/GuideLayout.svelte';
+import NextButton from '../../../../components/NextButton.svelte';
+import PrevButton from '../../../../components/PrevButton.svelte';
+</script>
