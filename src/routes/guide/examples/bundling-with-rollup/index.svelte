9ee9f0fa4c04982c4bb0baf9ba7662702bd3b731
phuocng <me@phuoc.ng>
2019-06-16 16:49:09 +0700

    Add Bundling with Rollup example

--- src/routes/guide/examples/bundling-with-rollup/index.svelte
+++ src/routes/guide/examples/bundling-with-rollup/index.svelte
@@ -1,13 +1,14 @@
 <svelte:head>
-	<title>FormValidation • Bundling with Webpack</title>
+	<title>FormValidation • Bundling with Rollup</title>
 </svelte:head>
 
 <GuideLayout>
-    <h1 class="tc lh-copy">Bundling with Webpack</h1>
+    <h1 class="tc lh-copy">Bundling with Rollup</h1>
 
     <section class="mv5">
-        <p class="lh-copy">FormValidation was rewritten in ES6 from v1.0.0. It comes with the <code>dist/es6</code> directory that consists of ES6 compatible classes. This guide shows how you can bundle FormValidation with <a href="https://webpack.js.org" class="blue dim link">Webpack</a>.</p>
+        <p class="lh-copy">FormValidation was rewritten in ES6 from v1.0.0. It comes with the <code>dist/es6</code> directory that consists of ES6 compatible classes. This guide shows how you can bundle FormValidation with <a href="https://rollupjs.org" class="blue dim link">Rollup</a>.</p>
         <p class="lh-copy">Assume that your folder has the following structure:</p>
+
 <SampleCode lang="html" code={`
 the-root-directory
     |
@@ -16,24 +17,33 @@ the-root-directory
     |   └── formvalidation
     |       └── dist
     |           └── es6
-    └── webpack.config.js
+    └── rollup.config.js
 `} />
         
-        <p class="lh-copy">Now, you can set the alias to FormValidation by adding the <code>resolve.alias</code> setting to <code>webpack.config.js</code>:</p>
+        <p class="lh-copy">Now, you can set the alias to FormValidation by installing the <a href="https://github.com/rollup/rollup-plugin-alias" class="blue dim link">rollup-plugin-alias</a> plugin:</p>
+        
+<SampleCode lang="html" code={`
+// Run this command from the root directory
+$ npm install --save-dev rollup-plugin-alias
+`} />
+
+        <p class="lh-copy">and using it in <code>rollup.config.js</code>:</p>
 <SampleCode lang="javascript" code={`
+import alias from 'rollup-plugin-alias';
 const path = require('path');
 
 module.exports = {
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            formvalidation: path.resolve(__dirname, '../vendors/formvalidation'),
-        },
-    },
+    plugins: [
+        alias({
+            formvalidation: path.resolve(__dirname, 'vendors/formvalidation'),
+        }),
+    ],
     ...
 };
 `} />
+
         <p class="lh-copy">From now on, you can import any file from FormValidation ES6 package as following:</p>
+
 <SampleCode lang="javascript" code={`
 // Use the algorithms
 import luhn from 'formvalidation/dist/es6/luhn';
