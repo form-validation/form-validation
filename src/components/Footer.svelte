2ca82a9c65f65843d96bc0473de4be9bffe7bcee
phuocng <me@phuoc.ng>
2019-07-09 21:40:58 +0700

    Tweak footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,25 +1,27 @@
-<div class="mw8 flex center">
-    <div class="w-40">
-        <div class="f4 fw6 mv3">FormValidation</div>
-        <ul class="list ma0 pa0 lh-copy">
-            <li><a href="/guide/getting-started" class="link">Getting Started</a></li>
-            <li><a href="/guide/validators" class="link">Validators</a></li>
-            <li><a href="/guide/plugins" class="link">Plugins</a></li>
-            <li><a href="/guide/api" class="link">API</a></li>
-            <li><a href="/guide/events" class="link">Events</a></li>
-            <li><a href="/guide/examples" class="link">Examples</a></li>
-        </ul>
+<div class="mw8 center">
+    <div class="flex">
+        <div class="w-50 pr3">
+            <div class="f4 fw6 mv3">FormValidation</div>
+            <ul class="list ma0 pa0 lh-copy">
+                <li><a href="/guide/getting-started" class="link">Getting Started</a></li>
+                <li><a href="/guide/validators" class="link">Validators</a></li>
+                <li><a href="/guide/plugins" class="link">Plugins</a></li>
+                <li><a href="/guide/api" class="link">API</a></li>
+                <li><a href="/guide/events" class="link">Events</a></li>
+                <li><a href="/guide/examples" class="link">Examples</a></li>
+            </ul>
+        </div>
+        <div class="w-50">
+            <div class="f4 fw6 mv3">Products</div>
+            <ul class="list ma0 pa0 lh-copy">
+                <li><a href="https://blur.page" class="link" target="_blank" title="BlurPage ~ A browser extension to blur any element or paragraph on page">BlurPage</a> ~ A browser extension to blur any element or paragraph on page</li>
+            </ul>
+        </div>
     </div>
-    <div class="w-30">
-        <div class="f4 fw6 mv3">Products</div>
-        <ul class="list ma0 pa0 lh-copy">
-            <li><a href="https://blur.page" class="link" target="_blank">BlurPage</a></li>
-        </ul>
-    </div>
-    <div class="w-30">
+    <div class="mt5">
         <div class="f4 fw6 mv3">About</div>
-        <div class="pb2">&copy; 2013 &mdash; 2019 Nguyen Huu Phuoc</div>
-        <div class="pb2">all rights reserved.</div>
+        <div class="pb2">&copy; 2013 &mdash; 2019 Nguyen Huu Phuoc. All rights reserved.</div>
+        <div class="pb2">This site is powered by <a href="https://svelte.dev" class="link" target="_blank">Svelte 3</a>.</div>
         <div class="flex items-center">
             <a href="https://twitter.com/nghuuphuoc" class="dib dim link black-70 ba b-black-70 br-pill w2 h2 flex items-center justify-center mr2">
                 <svg class="w-60 h-60" viewBox="0 0 24 24" style="stroke: rgb(0, 0, 0); fill: none; stroke-width: 1; stroke-linecap: round; stroke-linejoin: round"><path d="M23,6.628l-2-0.5l1-2l-2.464,0.7c-1.809-1.688-4.644-1.589-6.332,0.22c-0.78,0.836-1.21,1.938-1.204,3.08v1 c-3.539,0.73-6.634-1.2-9.5-4.5c-0.5,2.667,0,4.667,1.5,6l-3-0.5c0.405,2.069,1.362,3.7,4,4l-2.5,1c1,2,2.566,2.31,5,2.5 c-1.893,1.353-4.174,2.054-6.5,2c12.755,5.669,20-2.664,20-10V8.3L23,6.628z"></path></svg>
