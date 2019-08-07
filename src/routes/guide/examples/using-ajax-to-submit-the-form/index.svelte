7876b37089b0e865c96c1b6facee551fcffe8f8b
phuocng <me@phuoc.ng>
2019-08-07 21:33:49 +0700

    Add ajax uploading section

--- src/routes/guide/examples/using-ajax-to-submit-the-form/index.svelte
+++ src/routes/guide/examples/using-ajax-to-submit-the-form/index.svelte
@@ -43,7 +43,7 @@ FormValidation
         });
     });
 `} />
-        <p class="lh-copy">The sample code above uses a built in method <code>FormValidation.utils.fetch(url, options)</code> to send data to given <code>url</code>. It's up to you to choose your favourite library for doing the same thing, such as <a href="https://github.com/axios/axios" class="blue dim link">axios</a>:</p>
+        <p class="lh-copy">The sample code above uses a built in method <code>FormValidation.utils.fetch(url, options)</code> to send data to given <code>url</code>. It's up to you to choose your favourite library for doing the same thing, such as <a href="https://github.com/axios/axios" target="_blank" class="blue dim link">axios</a>:</p>
 <SampleCode lang="html" code={`
 <script-tag src="https://unpkg.com/axios/dist/axios.min.js"></script-tag>
 ...
@@ -74,6 +74,62 @@ document.addEventListener('DOMContentLoaded', function(e) {
 `} />
     </section>
 
+    <section class="mv5">
+        <Heading>Uploading file</Heading>
+
+        <p class="lh-copy">You can use the <a href="https://github.com/axios/axios" target="_blank" class="blue dim link">axios</a> library to send file to your back-end as well.</p>
+<SampleCode lang="html" code={`
+<form id="demoForm" method="POST">
+    <input type="file" name="avatar" />
+    ...
+</form>    
+
+<script-tag src="https://unpkg.com/axios/dist/axios.min.js"></script-tag>
+...
+<script-tag>
+document.addEventListener('DOMContentLoaded', function(e) {
+    FormValidation
+        .formValidation(
+            document.getElementById('demoForm'),
+            {
+                ...
+            }
+        )
+        .on('core.form.valid', function() {
+            var formData = new FormData();
+
+            // Append the text fields
+            formData.append('firstName', demoForm.querySelector('[name="firstName"]').value);
+            formData.append('lastName', demoForm.querySelector('[name="lastName"]').value);
+            formData.append('username', demoForm.querySelector('[name="username"]').value);
+            formData.append('email', demoForm.querySelector('[name="email"]').value);
+
+            // Append the file
+            var avatarFiles = demoForm.querySelector('[name="avatar"]').files;
+            if (avatarFiles.length > 0) {
+                formData.append('avatar', avatarFiles[0]);
+            }
+
+            axios.post('/path/to/your/back-end/', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then(function(response) {
+                ...
+            });
+        });
+});
+</script-tag>
+`} />        
+        <p class="lh-copy">The back-end then can get the uploaded file. The following sample code in PHP demonstrates how we can do that:</p>
+<SampleCode lang="php" code={`
+&lt;?php
+$targetFile = __DIR__ . '/uploads/' . basename($_FILES["avatar"]["name"]);
+$success = move_uploaded_file($_FILES["avatar"]["tmp_name"], $targetFile);
+...
+`} />
+    </section>
+
     <Examples examples={[
         'Multiple steps wizard',
         'Using Ajax to submit the form',
@@ -84,6 +140,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 <script>
 import Examples from '../../../../components/Examples.svelte';
 import GuideLayout from '../../../../components/GuideLayout.svelte';
+import Heading from '../../../../components/Heading.svelte';
 import RelatedValidators from '../../../../components/RelatedValidators.svelte';
 import SampleCode from '../../../../components/SampleCode.svelte';
 </script>
