2de2c73352126b0951283cdecfd93ccd6d8a46df
Phuoc Nguyen <me@phuoc.ng>
2020-10-01 22:55:11 +0700

    camelCaseToDash works with number (Recaptcha3Token for example)

--- src/components/helpers/camelCaseToDash.js
+++ src/components/helpers/camelCaseToDash.js
@@ -1,3 +1,3 @@
 export default (s) => {
-    return s.replace(/ /g, '').replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+    return s.replace(/ /g, '').replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
 };
