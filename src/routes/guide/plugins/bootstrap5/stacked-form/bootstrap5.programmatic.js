9024f5f7e052dd187a95f503efdd5eb5808854d7
Phuoc Nguyen <me@phuoc.ng>
2021-05-16 23:51:07 +0700

    Fix incorrect CSS path of Bootstrap 5

--- src/routes/guide/plugins/bootstrap5/stacked-form/bootstrap5.programmatic.js
+++ src/routes/guide/plugins/bootstrap5/stacked-form/bootstrap5.programmatic.js
@@ -2,7 +2,7 @@ export default `<!doctype html>
 <html>
 <head>
     <link-tag rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link-tag rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link-tag rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
     <link-tag rel="stylesheet" href="/vendors/formvalidation/dist/css/formValidation.min.css">
 </head>
 <body>
